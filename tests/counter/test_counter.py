7043d04f76a2840b91e8d0b878c6483f4d34dc70
Lucas Giolo <giololucas@gmail.com>
2024-06-07 07:28:55 -0300

    test: altera teste de counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,11 +1,8 @@
 from src.pre_built.counter import count_ocurrences
 
 JOBS_DATAPATH = "data/jobs.csv"
-WORDS_DATAPATH = "tests/mocks/words.csv"
 
 
 def test_counter():
     assert count_ocurrences(JOBS_DATAPATH, "Python") == 1639
     assert count_ocurrences(JOBS_DATAPATH, "Javascript") == 122
-    assert count_ocurrences(WORDS_DATAPATH, "Python") == 3
-    assert count_ocurrences(WORDS_DATAPATH, "Javascript") == 2
