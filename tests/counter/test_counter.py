d1f4ef7dcc0c5b0d209223655796ff4765db00ee
Lucas Giolo <giololucas@gmail.com>
2024-06-07 07:14:43 -0300

    fix: Corrige espaçamento

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -3,6 +3,7 @@ from src.pre_built.counter import count_ocurrences
 JOBS_DATAPATH = "data/jobs.csv"
 WORDS_DATAPATH = "tests/mocks/words.csv"
 
+
 def test_counter():
     assert count_ocurrences(JOBS_DATAPATH, "Python") == 1639
     assert count_ocurrences(JOBS_DATAPATH, "Javascript") == 122
