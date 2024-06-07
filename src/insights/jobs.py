5f7eb29a5cbc93340f999f6e1f59c0116518612a
Lucas Giolo <giololucas@gmail.com>
2024-06-07 07:06:34 -0300

    feat: Implementa o método read

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,12 +1,13 @@
 from typing import List, Dict
-
+import csv
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, filepath: str) -> List[Dict]:
+        with open(filepath, "r") as file:
+            self.jobs_list = list(csv.DictReader(file))
 
     def get_unique_job_types(self) -> List[str]:
         pass
