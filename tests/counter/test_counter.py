49320b8184ae655a62b6a3e29efc3d9213536c5b
Lucas Giolo <giololucas@gmail.com>
2024-06-07 06:26:08 -0300

    Commit inicial

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
+JOBS_DATAPATH = "data/jobs.csv"
+WORDS_DATAPATH = "tests/mocks/words.csv"
 
 def test_counter():
-    raise
+    assert count_ocurrences(JOBS_DATAPATH, "Python") == 1639
+    assert count_ocurrences(JOBS_DATAPATH, "Javascript") == 122
+    assert count_ocurrences(WORDS_DATAPATH, "Python") == 3
+    assert count_ocurrences(WORDS_DATAPATH, "Javascript") == 2
