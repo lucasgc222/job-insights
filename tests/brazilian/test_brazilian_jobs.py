3cf77952a54d720a27001e015df40686351006b8
Lucas Giolo <giololucas@gmail.com>
2024-06-07 23:55:52 -0300

    feat: Cria teste para read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,12 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    raise
+    path = "tests/mocks/brazilians_jobs.csv"
+
+    jobs = read_brazilian_file(path)
+    print(jobs)
+    for job in jobs:
+        assert "title" in job
+        assert "salary" in job
+        assert "type" in job
